dc11b76b2ae4a913860ed40123353e151285b795
Jarcionek <jarcionek@users.noreply.github.com>
2019-03-23 23:28:45 +0000

    changed default preset values to a more distinct set

--- settings.lua
+++ settings.lua
@@ -5,7 +5,7 @@ data:extend({
 		type = "double-setting",
 		name = "ZoomPresets_preset-1-zoom-level",
 		setting_type = "runtime-per-user",
-        default_value = 3.0,
+        default_value = 1.0,
 		order = "1a"
 	},
 	{
@@ -20,7 +20,7 @@ data:extend({
 		type = "double-setting",
 		name = "ZoomPresets_preset-2-zoom-level",
 		setting_type = "runtime-per-user",
-		default_value = 0.275,
+		default_value = 0.2,
 		order = "2a"
 	},
 	{
@@ -35,14 +35,14 @@ data:extend({
 		type = "double-setting",
 		name = "ZoomPresets_preset-3-zoom-level",
 		setting_type = "runtime-per-user",
-        default_value = 3.0,
+        default_value = 0.15,
 		order = "3a"
 	},
 	{
 		type = "string-setting",
 		name = "ZoomPresets_preset-3-view-type",
 		setting_type = "runtime-per-user",
-		default_value = choices.view_option.player,
+		default_value = choices.view_option.zoomToWorldView,
 		allowed_values = choices.view_option,
 		order = "3b"
 	},
@@ -50,14 +50,14 @@ data:extend({
 		type = "double-setting",
 		name = "ZoomPresets_preset-4-zoom-level",
 		setting_type = "runtime-per-user",
-		default_value = 0.275,
+		default_value = 0.003,
 		order = "4a"
 	},
 	{
 		type = "string-setting",
 		name = "ZoomPresets_preset-4-view-type",
 		setting_type = "runtime-per-user",
-		default_value = choices.view_option.player,
+		default_value = choices.view_option.mapView,
 		allowed_values = choices.view_option,
 		order = "4b"
 	}
