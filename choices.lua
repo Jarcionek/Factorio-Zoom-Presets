b8e475ed479b91a3b3937ddc07204991eeb89718
Jarcionek <jarcionek@users.noreply.github.com>
2019-03-23 22:32:02 +0000

    added settings with dropdowns for the view type

---
+++ choices.lua
@@ -0,0 +1,7 @@
+return {
+    view_option = {
+        player = "Player",
+        zoomToWorldView = "Zoom to World View",
+        mapView = "Map View",
+    },
+}
--- settings.lua
+++ settings.lua
@@ -1,14 +1,34 @@
+local choices = require("choices")
+
 data:extend({
 	{
 		type = "double-setting",
 		name = "ZoomPresets_preset-1-zoom-level",
 		setting_type = "runtime-per-user",
-        default_value = 3.0
+        default_value = 3.0,
+		order = "1a"
+	},
+	{
+		type = "string-setting",
+		name = "ZoomPresets_preset-1-view-type",
+		setting_type = "runtime-per-user",
+		default_value = choices.view_option.player,
+		allowed_values = choices.view_option,
+		order = "1b"
 	},
 	{
 		type = "double-setting",
 		name = "ZoomPresets_preset-2-zoom-level",
 		setting_type = "runtime-per-user",
-		default_value = 0.275
-	}
+		default_value = 0.275,
+		order = "2a"
+	},
+	{
+		type = "string-setting",
+		name = "ZoomPresets_preset-2-view-type",
+		setting_type = "runtime-per-user",
+		default_value = choices.view_option.player,
+		allowed_values = choices.view_option,
+		order = "2b"
+	},
 })
