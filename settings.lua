cf08e8a9c64512121cac6fb44c77ac31500e6a1d
Jarcionek <jarcionek@users.noreply.github.com>
2019-03-24 18:51:12 +0000

    added minimum and maximum values for zoom level (which are far far greater than in vanilla game)

--- settings.lua
+++ settings.lua
@@ -1,64 +1,72 @@
 local choices = require("choices")
 
 data:extend({
-	{
-		type = "double-setting",
-		name = "ZoomPresets_preset-1-zoom-level",
-		setting_type = "runtime-per-user",
+    {
+        type = "double-setting",
+        name = "ZoomPresets_preset-1-zoom-level",
+        setting_type = "runtime-per-user",
         default_value = 1.0,
-		order = "1a"
-	},
-	{
-		type = "string-setting",
-		name = "ZoomPresets_preset-1-view-type",
-		setting_type = "runtime-per-user",
-		default_value = choices.view_option.player,
-		allowed_values = choices.view_option,
-		order = "1b"
-	},
-	{
-		type = "double-setting",
-		name = "ZoomPresets_preset-2-zoom-level",
-		setting_type = "runtime-per-user",
-		default_value = 0.2,
-		order = "2a"
-	},
-	{
-		type = "string-setting",
-		name = "ZoomPresets_preset-2-view-type",
-		setting_type = "runtime-per-user",
-		default_value = choices.view_option.player,
-		allowed_values = choices.view_option,
-		order = "2b"
-	},
-	{
-		type = "double-setting",
-		name = "ZoomPresets_preset-3-zoom-level",
-		setting_type = "runtime-per-user",
+        maximum_value = 10.0,
+        minimum_value = 0.0001,
+        order = "1a"
+    },
+    {
+        type = "string-setting",
+        name = "ZoomPresets_preset-1-view-type",
+        setting_type = "runtime-per-user",
+        default_value = choices.view_option.player,
+        allowed_values = choices.view_option,
+        order = "1b"
+    },
+    {
+        type = "double-setting",
+        name = "ZoomPresets_preset-2-zoom-level",
+        setting_type = "runtime-per-user",
+        default_value = 0.2,
+        maximum_value = 10.0,
+        minimum_value = 0.0001,
+        order = "2a"
+    },
+    {
+        type = "string-setting",
+        name = "ZoomPresets_preset-2-view-type",
+        setting_type = "runtime-per-user",
+        default_value = choices.view_option.player,
+        allowed_values = choices.view_option,
+        order = "2b"
+    },
+    {
+        type = "double-setting",
+        name = "ZoomPresets_preset-3-zoom-level",
+        setting_type = "runtime-per-user",
         default_value = 0.15,
-		order = "3a"
-	},
-	{
-		type = "string-setting",
-		name = "ZoomPresets_preset-3-view-type",
-		setting_type = "runtime-per-user",
-		default_value = choices.view_option.zoomToWorldView,
-		allowed_values = choices.view_option,
-		order = "3b"
-	},
-	{
-		type = "double-setting",
-		name = "ZoomPresets_preset-4-zoom-level",
-		setting_type = "runtime-per-user",
-		default_value = 0.003,
-		order = "4a"
-	},
-	{
-		type = "string-setting",
-		name = "ZoomPresets_preset-4-view-type",
-		setting_type = "runtime-per-user",
-		default_value = choices.view_option.mapView,
-		allowed_values = choices.view_option,
-		order = "4b"
-	}
+        maximum_value = 10.0,
+        minimum_value = 0.0001,
+        order = "3a"
+    },
+    {
+        type = "string-setting",
+        name = "ZoomPresets_preset-3-view-type",
+        setting_type = "runtime-per-user",
+        default_value = choices.view_option.zoomToWorldView,
+        allowed_values = choices.view_option,
+        order = "3b"
+    },
+    {
+        type = "double-setting",
+        name = "ZoomPresets_preset-4-zoom-level",
+        setting_type = "runtime-per-user",
+        default_value = 0.003,
+        maximum_value = 10.0,
+        minimum_value = 0.0001,
+        order = "4a"
+    },
+    {
+        type = "string-setting",
+        name = "ZoomPresets_preset-4-view-type",
+        setting_type = "runtime-per-user",
+        default_value = choices.view_option.mapView,
+        allowed_values = choices.view_option,
+        order = "4b"
+    }
 })
